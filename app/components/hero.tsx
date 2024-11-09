875fa295041d40e3f4cfcbd75452b1fbc0d883b0
Rizkyhu14 <rizkyhaikal1134@gmail.com>
2024-11-09 22:01:20 +0700

    Add files via upload

---
+++ app/components/hero.tsx
@@ -0,0 +1,25 @@
+import fotosaya from "../rizky.jpg"
+
+function Profile() {
+    return (
+      <img
+        src={fotosaya.src}
+        alt="rizky"
+        className="fotoku"
+      />
+    );
+  }
+
+export default function Hero(){
+    return(
+        <div className="container mx-auto p-2 text-center">
+        <h1 className="text-black-300 font-bold">CV Onlne</h1>
+        <h2 className="text-3x1">Rizky Haikal Umaro</h2>
+        <Profile />
+        <p>
+        Saya adalah seorang mahasiswa Masoem University angkatan 2022 
+        jurusan Komputerisasi Akuntansi. Sebelumnya saya bersekolah di SMA Al-Masoem.
+        </p>
+      </div>
+    )
+}
